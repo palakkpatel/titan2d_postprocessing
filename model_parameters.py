a29a9ffd6c5630f324a69d36ce2c2b7438c8b169
Palak Patel <palakpatel910@gmail.com>
2023-10-18 17:36:31 -0400

    Updated model_parameters.py to read argument to respective variables

---
+++ model_parameters.py
@@ -0,0 +1,18 @@
+from array import array
+import sys
+
+# ks = 1.389e-6
+# manning = 0.05
+# lambda_basal=0.6
+# F = 0.008
+# DS = 1e-5
+
+ks = float(sys.argv[1])
+manning = float(sys.argv[2])
+lambda_basal = float(sys.argv[3])
+F = float(sys.argv[4])
+DS = float(sys.argv[5])
+R_coef = float(sys.argv[6])
+
+#print(type(ks))
+
