305ffe50769bdf6913793fdb77c92d72f6d6fc92
Palak Patel <palakpatel910@gmail.com>
2023-10-19 09:03:34 -0400

    Added file to run single sim on titan with debris flow

---
+++ run_titan_debrisflow.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# Run this script in your working Dir
+
+# Six Parameters to Update
+para1=2.925e-06
+para2=0.428388
+para3=0.16691
+para4=8.92598e-04
+para5=6.90772e-05
+para6=1.2558
+
+# Number of Processors to Run
+NPROCS=8
+
+# Path to Titan
+export titan=/cluster/tufts/patralab/ppatel20/titan2d/titan_wsp/titan2d_bld/iccoptompmpi/bin/titan
+
+#Path to model_parameters.py
+model_parameters="../model_parameters.py"
+
+# Run model_parameters.py
+python3 model_parameters.py $para1 $para2 $para3 $para4 $para5 $para6
+
+# Run Titan2d
+$titan -nt $NPROCS input.py
+
+
