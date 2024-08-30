6c1170e93d99bd930aab9eec6b3b9302d4151c61
Palak Patel <palakpatel910@gmail.com>
2024-08-30 11:43:56 -0400

    Updated run_ensemble script

--- run_ensemble.sh
+++ run_ensemble.sh
@@ -1,27 +1,24 @@
 #! /bin/bash
 
-parameter_design_file='Latin_model_parameters_mask.txt'
-
-n=1
-
-# Starting Simulation Number from the File
-n_start=61
-
-# Last Simulation to run (inclusive)
-n_stop=65
+# Update User Define Parameters
+PARAMETER_DESIGN_FILE='Latin_model_parameters_mask.txt'
+SIM_START_N=61 # Starting Simulation Number from the File
+SIM_STOP_N=65 # Last Simulation to run (inclusive)
 
 # Simulation Template DIR
 temp_sim="../temp_sim"
 
 # Simulation DIR Prefix
-sim_dir="sim_noAMR"
+SIM_DIR_PREFIX="sim"
 
 # Slurm File Name
-slurm_file="titan_run.slurm"
+SLURM_FILE_PATH="titan_run.slurm"
 
 # Path to model_parameters.py
 model_parameters="../model_parameters.py"
 
+n=1
+# Looping over Parameter Design File
 while read line; do
     line=$(echo $line | sed 's/\,/ /g')
     para1=$(echo $line | awk '{print $1}')
@@ -31,22 +28,24 @@ while read line; do
     para5=$(echo $line | awk '{print $5}')
     para6=$(echo $line | awk '{print $6}')
 
-    if [ $n -gt $((n_start-1)) ]
+    if [ $n -gt $((SIM_START_N-1)) ]
     then
-        python3 $model_parameters $para1 $para2 $para3 $para4 $para5 $para6
-        foldername="${sim_dir}_${n}"
-        mkdir $foldername
-        mv parameters.bin $foldername/
-        cd $foldername
+        # Exceuting Model Parameters File 
+        python $model_parameters $para1 $para2 $para3 $para4 $para5 $para6
+        sim_dir_name="${SIM_DIR_PREFIX}_${n}"
+        mkdir $sim_dir_name
+        mv parameters.bin $sim_dir_name/
+        cd $sim_dir_name
         cp -r $temp_sim/* ./
-        sbatch $slurm_file
+        sbatch $SLURM_FILE_PATH
         cd ..
     fi
-    #echo "Line no: $n = $line"
+
     n=$((n+1))
-    if [ $n -gt $n_stop ]
+    if [ $n -gt $SIM_STOP_N ]
     then
         break
     fi
-done < $parameter_design_file
+done < $PARAMETER_DESIGN_FILE
 
+echo "Done"
